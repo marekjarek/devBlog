050c411778a8e29095953d3562eab72a8298e305
Markuz <krupa_marek@yahoo.com>
2017-07-17 13:06:25 +0200

    The nav complete

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,3 +9,14 @@ Vue.use(Buefy);
 
 
 //Vue.component('example', require('./components/Example.vue'));
+
+var app = new Vue({
+  el: '#app',
+  data: {}
+});
+
+$(document).ready(function() {
+  $('button.dropdown').hover(function(e) {
+    $(this).toggleClass('is-open');
+  });
+});
